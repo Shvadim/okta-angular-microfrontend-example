3699497c4ca179330ed5a443ec9e96dd11d88dc3
Shvadim <shvadst@gmail.com>
2023-05-22 23:12:54 +0300

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	new file:   projects/mfe-profile/.browserslistrc 	new file:   projects/mfe-profile/karma.conf.js 	new file:   projects/mfe-profile/src/app/app-routing.module.ts 	new file:   projects/mfe-profile/src/app/app.component.html 	new file:   projects/mfe-profile/src/app/app.component.ts 	new file:   projects/mfe-profile/src/app/app.module.ts 	new file:   projects/mfe-profile/src/app/home/home.component.html 	new file:   projects/mfe-profile/src/app/home/home.component.ts 	new file:   projects/mfe-profile/src/app/profile/profile-routing.module.ts 	new file:   projects/mfe-profile/src/app/profile/profile.component.html 	new file:   projects/mfe-profile/src/app/profile/profile.component.ts 	new file:   projects/mfe-profile/src/app/profile/profile.module.ts 	new file:   projects/mfe-profile/src/assets/.gitkeep 	new file:   projects/mfe-profile/src/bootstrap.ts 	new file:   projects/mfe-profile/src/environments/environment.prod.ts 	new file:   projects/mfe-profile/src/environments/environment.ts 	new file:   projects/mfe-profile/src/favicon.ico 	new file:   projects/mfe-profile/src/index.html 	new file:   projects/mfe-profile/src/main.ts 	new file:   projects/mfe-profile/src/polyfills.ts 	new file:   projects/mfe-profile/src/styles.css 	new file:   projects/mfe-profile/src/test.ts 	new file:   projects/mfe-profile/tsconfig.app.json 	new file:   projects/mfe-profile/tsconfig.spec.json 	new file:   projects/mfe-profile/webpack.config.js 	new file:   projects/mfe-profile/webpack.prod.config.js 	modified:   projects/shell/src/app/app-routing.module.ts 	modified:   projects/shell/src/app/app.component.html 	modified:   projects/shell/src/app/app.component.ts 	modified:   projects/shell/src/app/app.module.ts 	deleted:    projects/shell/src/decl.d.ts 	modified:   projects/shell/webpack.config.js 	modified:   tsconfig.json

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
-import { ProductComponent } from './products/product.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductsComponent } from "./products/products.component";
+import { ProductComponent } from "./products/product.component";
+
+import { OKTA_CONFIG, OktaAuthModule } from "@okta/okta-angular";
+import { OktaAuth } from "@okta/okta-auth-js";
+
+const oktaAuth = new OktaAuth({
+  issuer: "https://dev-36355745.okta.com/oauth2/default",
+  clientId: "0oa9mrvteo1PzhCeu5d7",
+  redirectUri: window.location.origin + "/login/callback",
+  scopes: ["openid", "profile", "email"],
+});
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductsComponent,
-    ProductComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductsComponent, ProductComponent],
+  imports: [BrowserModule, AppRoutingModule, OktaAuthModule],
+  providers: [{ provide: OKTA_CONFIG, useValue: { oktaAuth } }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
